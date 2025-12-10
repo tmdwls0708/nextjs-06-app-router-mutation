6a49516223f631e2d807d3a3956151870d271166
sjkim <07nevergiveup08@gmail.com>
2025-12-10 20:44:25 +0900

    폼 전송 액션 추가 및 유효성 검증 로직 추가

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,6 +1,10 @@
+"use client";
+
 import FormSubmit from "@/components/form-submit";
 import { storePost } from "@/lib/posts";
 import { redirect } from "next/navigation";
+import { useActionState } from "react";
+// import { useFormState } from "react-dom";
 
 export default function NewPostPage() {
   async function createPost(formData) {
@@ -8,6 +12,22 @@ export default function NewPostPage() {
     const title = formData.get("title");
     const image = formData.get("image");
     const content = formData.get("content");
+
+    let errors = [];
+
+    if (!title || title.trim().length === 0) {
+      errors.push("Title is required");
+    }
+    if (!content || content.trim().length === 0) {
+      errors.push("Content is required");
+    }
+    if (!image) {
+      errors.push("Image is required");
+    }
+
+    if (errors.length > 0) {
+      return { errors };
+    }
 
     await storePost({
       imageUrl: "",
@@ -19,13 +39,15 @@ export default function NewPostPage() {
     redirect("/feed");
   }
 
+  const [state, formAction] = useActionState(createPost, {});
+
   return (
     <>
       <h1>Create a new post</h1>
-      <form action={createPost}>
+      <form action={formAction}>
         <p className="form-control">
           <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" />
+          <input type="text" id="title" name="title" required />
         </p>
         <p className="form-control">
           <label htmlFor="image">Image URL</label>
